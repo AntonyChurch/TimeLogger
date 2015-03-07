2dcd9151f2d291b46eb0a41deefd4fac32843f9f
Antony Church <achurch44.ac@gmail.com>
2015-03-07 22:06:50 +0000

    Added functionality to let users calculate time difference between two times. It's still in a prototyping phase at the moment

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # TimeLogger
 Erlang console based time logger.
 
+#To Use
+This software is written with rebar so all compilation and running can be done the standard way.
+1. Call timelogger:start(). Storing the return value, it is the Table Id used throughout the application.
+2. call timelogger:logtime(TableId, name, description). Do this as many times as needed.
+3. To log all of your time call timelogger:viewall(TableId).
+
 ##Todo List
 1. Migrate implement using OTP Server
 2. Get time between tasks calculated
--- src/timelogger.erl
+++ src/timelogger.erl
@@ -1,5 +1,5 @@
 -module(timelogger).
--export([start/0, logtime/3, viewall/1, calculatetimes/2]).
+-export([start/0, logtime/3, viewall/1, calculatetime/3, findtime/2]).
 
 start() ->
     ets:new(times, [duplicate_bag]).
@@ -7,8 +7,16 @@ start() ->
 logtime(TableId, Code, Desc) ->
     ets:insert(TableId, {Code, Desc, erlang:time()}).
 
+calculatetime(TableId, Code1, Code2) ->
+    {_, _, Time1} = hd(ets:lookup(TableId, Code1)),
+    {_, _, Time2} = hd(ets:lookup(TableId, Code2)),
+    calculatetimes(Time1, Time2).
+
 viewall(TableId) ->
     ets:tab2list(TableId).
+
+findtime(TableId, Code) ->
+    ets:lookup(TableId, Code).
 
 calculatetimes(Time1, Time2) ->
     {A, B, C} = Time1,
