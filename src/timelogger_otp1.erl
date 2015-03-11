2e057b9922963e003620e9f264cb7e885dc671c6
Antony Church <antony.church@strawberry.co.uk>
2015-03-11 14:56:21 +0000

    Added time difference calculation.

--- src/timelogger_otp1.erl
+++ src/timelogger_otp1.erl
@@ -6,7 +6,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 %% Usage Functions
--export([logtime/2, viewall/0]).
+-export([logtime/2, viewall/0, calculatediff/2]).
 
 %% Defines
 -define(SERVER, ?MODULE).
@@ -23,8 +23,16 @@ logtime(Code, Desc) ->
 viewall() ->
     gen_server:call(?MODULE, viewall).
 
+calculatediff(Code1, Code2) ->
+		gen_server:call(?MODULE, {calculatediff, Code1, Code2}).
+
+calculatetimes(Time1, Time2) ->
+    {A, B, C} = Time1,
+    {X, Y, Z} = Time2,
+    {A - X, B - Y, C - Z}.
+
 init([]) ->
-		{ok, Ref} = dets:open_file(test, []),
+		{ok, Ref} = dets:open_file(timelog, []),
 		{ok, Ref}.
 
 handle_call({logtime, Code, Desc}, _From, Table) ->
@@ -35,6 +43,11 @@ handle_call(viewall, _From, Table) ->
     Reply = ets:tab2list(dets:to_ets(Table, EtsTable)),
 		ets:delete(EtsTable),
     {reply, Reply, Table};
+handle_call({calculatediff, Code1, Code2}, _From, Table) ->
+		{_, _, Time1} = hd(dets:lookup(Table, Code1)),
+		{_, _, Time2} = hd(dets:lookup(Table, Code2)),
+		Reply = calculatetimes(Time1, Time2),
+		{reply, Reply, Table};
 handle_call(stop, _From, Table) ->
 		dets:close(Table),
     {stop, normal, stopped, Table}.
